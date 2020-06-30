860a5e90fa1eed467735662a330033c26d366874
haesooKr <gotn676@gmail.com>
2020-06-30 15:53:38 -0400

    Update Child Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import NewsList from './components/NewsList';
+import Categories from './components/Categories';
 
 const App = () => {
   return (
     <div>
+      <Categories />
       <NewsList />
     </div>
   );
