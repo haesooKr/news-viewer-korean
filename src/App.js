f44faa511a38d7185cdcf21850e706dbad6b8bb4
haesooKr <gotn676@gmail.com>
2020-06-30 16:15:14 -0400

    Add Category Route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,9 @@
-import React, { useState, useCallback } from 'react';
-import axios from 'axios';
-import NewsList from './components/NewsList';
-import Categories from './components/Categories';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import NewsPage from './pages/NewsPage';
 
 const App = () => {
-  const [category, setCategory] = useState('all');
-  
-  const onSelect = useCallback(category => setCategory(category), []);
-  return (
-    <div>
-      <Categories category={category} onSelect={onSelect}/>
-      <NewsList category={category}/>
-    </div>
-  );
+  return <Route path="/:category?" component={NewsPage} />;
 };
 
 export default App;
