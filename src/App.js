f42f871ea31f907bc4bc967a3a1cc1f8bfe9fa8f
haesooKr <gotn676@gmail.com>
2020-06-30 15:46:14 -0400

    Link NewList Component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import NewsList from './components/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-   try {
-    const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=a29cc728ce39493994a21215cbf1035c');
-    setData(response); 
-   } catch (e) {
-     console.log(e);
-   }
-  }
   return (
     <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>  
-  { data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true}/> }
+      <NewsList />
     </div>
   );
 };
