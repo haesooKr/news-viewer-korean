c3d34d4a6cdaf61e74800cfc72725b7c32639ef8
haesooKr <gotn676@gmail.com>
2020-06-30 15:44:57 -0400

    Create NewsList Component

---
+++ src/components/NewsList.js
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from 'react';
+import NewsItem from './NewsItem';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`;
+
+const sampleArticle = {
+  title: 'title',
+  description: 'desc',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160',
+};
+
+const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=a29cc728ce39493994a21215cbf1035c',
+        );
+        console.log(response);
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기 중...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  );
+};
+
+export default NewsList;
